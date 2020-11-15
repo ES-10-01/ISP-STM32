dae24c81c183cf4b724ebdfb9d7a582063402b1e
ArtemVozzgaev <37670156+artem8449@users.noreply.github.com>
2020-11-15 21:19:37 +0300

    add freertos tasks

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -44,6 +45,48 @@
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
 
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for init_task */
+osThreadId_t init_taskHandle;
+const osThreadAttr_t init_task_attributes = {
+  .name = "init_task",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for close_state */
+osThreadId_t close_stateHandle;
+const osThreadAttr_t close_state_attributes = {
+  .name = "close_state",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for input_task */
+osThreadId_t input_taskHandle;
+const osThreadAttr_t input_task_attributes = {
+  .name = "input_task",
+  .priority = (osPriority_t) osPriorityBelowNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for wait_answer_tas */
+osThreadId_t wait_answer_tasHandle;
+const osThreadAttr_t wait_answer_tas_attributes = {
+  .name = "wait_answer_tas",
+  .priority = (osPriority_t) osPriorityBelowNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for open_task */
+osThreadId_t open_taskHandle;
+const osThreadAttr_t open_task_attributes = {
+  .name = "open_task",
+  .priority = (osPriority_t) osPriorityBelowNormal,
+  .stack_size = 128 * 4
+};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -52,6 +95,13 @@ UART_HandleTypeDef huart1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
+void StartDefaultTask(void *argument);
+void InitTask(void *argument);
+void CloseState(void *argument);
+void InputTask(void *argument);
+void WaitAnswerTask(void *argument);
+void OpenTask(void *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -117,24 +167,56 @@ int main(void)
 
   /* USER CODE END 2 */
 
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of init_task */
+  init_taskHandle = osThreadNew(InitTask, NULL, &init_task_attributes);
+
+  /* creation of close_state */
+  close_stateHandle = osThreadNew(CloseState, NULL, &close_state_attributes);
+
+  /* creation of input_task */
+  input_taskHandle = osThreadNew(InputTask, NULL, &input_task_attributes);
+
+  /* creation of wait_answer_tas */
+  wait_answer_tasHandle = osThreadNew(WaitAnswerTask, NULL, &wait_answer_tas_attributes);
+
+  /* creation of open_task */
+  open_taskHandle = osThreadNew(OpenTask, NULL, &open_task_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  char a = read_keypad();
-	  if (a=='2') {
-		  HAL_GPIO_WritePin(GPIOB, green_led_Pin, GPIO_PIN_SET);
-		  HAL_Delay(500);
-		  HAL_GPIO_WritePin(GPIOB, green_led_Pin, GPIO_PIN_RESET);
-	  }
-	  else if (a=='4') {
-		  HAL_GPIO_WritePin(GPIOB, blue_led_Pin, GPIO_PIN_SET);
-		  HAL_Delay(500);
-		  HAL_GPIO_WritePin(GPIOB, blue_led_Pin, GPIO_PIN_RESET);
-	  }
-	  if (check_esp_available(NULL, &huart1) != 0) {
-		  return 2;
-	  }
 
 	  return 0;
     /* USER CODE END WHILE */
@@ -259,6 +341,119 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_InitTask */
+/**
+* @brief Function implementing the init_task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_InitTask */
+void InitTask(void *argument)
+{
+  /* USER CODE BEGIN InitTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	  if (check_esp_available(NULL, &huart1) != 0) {
+	 		  return 2;
+	 	  }
+  }
+  /* USER CODE END InitTask */
+}
+
+/* USER CODE BEGIN Header_CloseState */
+/**
+* @brief Function implementing the close_state thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_CloseState */
+void CloseState(void *argument)
+{
+  /* USER CODE BEGIN CloseState */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END CloseState */
+}
+
+/* USER CODE BEGIN Header_InputTask */
+/**
+* @brief Function implementing the input_task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_InputTask */
+void InputTask(void *argument)
+{
+  /* USER CODE BEGIN InputTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_GPIO_WritePin(GPIOB, blue_led_Pin, GPIO_PIN_SET);
+	  char a = read_keypad();
+	  HAL_GPIO_WritePin(GPIOB, blue_led_Pin, GPIO_PIN_RESET);
+
+  }
+  /* USER CODE END InputTask */
+}
+
+/* USER CODE BEGIN Header_WaitAnswerTask */
+/**
+* @brief Function implementing the wait_answer_tas thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_WaitAnswerTask */
+void WaitAnswerTask(void *argument)
+{
+  /* USER CODE BEGIN WaitAnswerTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END WaitAnswerTask */
+}
+
+/* USER CODE BEGIN Header_OpenTask */
+/**
+* @brief Function implementing the open_task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_OpenTask */
+void OpenTask(void *argument)
+{
+  /* USER CODE BEGIN OpenTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_GPIO_WritePin(GPIOB, green_led_Pin, GPIO_PIN_SET);
+  }
+  /* USER CODE END OpenTask */
+}
 
  /**
   * @brief  Period elapsed callback in non blocking mode
