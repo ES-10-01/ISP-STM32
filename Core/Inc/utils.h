2cf81400a99ca46d27d5782d33887901a9329521
Viktor Khrulev <victorkhrulev@gmail.com>
2020-11-16 23:23:17 +0300

    Implement wifi lib for communication with server

---
+++ Core/Inc/utils.h
@@ -0,0 +1,14 @@
+/*
+ * utils.h
+ *
+ *  Created on: Nov 16, 2020
+ *      Author: demavag
+ */
+
+#ifndef INC_UTILS_H_
+#define INC_UTILS_H_
+
+#define RC_OK(rc) ((rc) >= 0)
+#define RC_FAIL(rc) (!(RC_OK(rc)))
+
+#endif /* INC_UTILS_H_ */
