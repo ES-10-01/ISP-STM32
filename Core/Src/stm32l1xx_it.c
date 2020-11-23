ff1bb3a797999dee5758e6bb2a5def150469539c
ArtemVozzgaev <37670156+artem8449@users.noreply.github.com>
2020-11-23 21:20:07 +0300

    add lcd library and printout need info about state

--- Core/Src/stm32l1xx_it.c
+++ Core/Src/stm32l1xx_it.c
@@ -66,7 +66,7 @@ extern TIM_HandleTypeDef htim2;
 /* USER CODE END EV */
 
 /******************************************************************************/
-/*           Cortex-M3 Processor Interruption and Exception Handlers          */ 
+/*           Cortex-M3 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
   * @brief This function handles Non maskable interrupt.
